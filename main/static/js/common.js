a8e4f3b3821c96eca1d64b9efcdcab6fcf608344
ItsukiNagao <nagaoituki@gmail.com>
2022-06-13 20:59:00 +0900

    type=fileの画像を即時プレビュー

--- main/static/js/common.js
+++ main/static/js/common.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded',() => {
 		event.target.classList.add('drag');
 	});
 
-	uploadArea.addEventlistener("dragleave",(event) => {
+	uploadArea.addEventListener("dragleave",(event) => {
 		event.target.classList.remove('drag');
 	});
 
@@ -31,6 +31,45 @@ document.addEventListener('DOMContentLoaded',() => {
 	uploadArea.addEventListener("drop",(event) => {
 		event.preventDefault();
 		var input = document.querySelectorAll('input[name="fileupload"]');
+		//取得したinput[type=file]にDropしたファイルを格納する。
 		input[0].files = event.dataTransfer.files;
+		OnFileSelect(input[0]);
 	});
 });
+
+function OnFileSelect( inputElement ) {
+  console.log("OnFileSelect()を開始します");
+  //ファイルリストを取得
+  let fileList = inputElement.files;
+  console.dir(fileList);
+
+  //ファイルの数を取得
+  let fileCount = fileList.length;
+
+  let loadCompleteCount = 0;
+  let imageList = "";
+
+  //選択されたファイルの数だけ処理する。
+  for ( let i = 0; i < fileCount; i++ ) {
+	//FileReaderクラスのインスタンス生成
+	const fileReader = new FileReader();
+
+	//ファイルを取得
+	const file = fileList[i];
+
+	//読み込み完了時の処理を追加
+	fileReader.onload = function() {
+	  //<li>,<img>タグの生成
+	  imageList += "<li><p>" + file.name + "</p></li>\r\n";
+	  imageList += "<li><img src=\"" + this.result + "\" /></li>\r\n";
+	  //選択されたファイル全ての処理が完了したら,<ul>タグに流し込む
+	  if ( ++loadCompleteCount == fileCount ) {
+		//<ul>タグに<li>,<img>を流し込む
+		document.getElementById( "ID001" ).innerHTML = imageList;
+	  }
+	};
+
+	//ファイルの読み込み(Data URI Schemeの取得)
+	fileReader.readAsDataURL( file );
+  }
+}
